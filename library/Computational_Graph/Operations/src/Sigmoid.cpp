9b7e1a11b370b1b895fbbd360db22349d623ce84
ga96tul <Freemander10>
2019-05-24 14:14:59 +0200

    Reactivated git submodules (Eigen,pybind11) and deactivated find_package(Eigen)

--- library/Computational_Graph/Operations/src/Sigmoid.cpp
+++ library/Computational_Graph/Operations/src/Sigmoid.cpp
@@ -5,16 +5,16 @@
 #include <iostream>
 #include "Sigmoid.hpp"
 
+
 void Sigmoid::forwards() {
 //TODO: Might try using fast sigmoid f(x) = x / (1 + abs(x))
 //https://stackoverflow.com/questions/10732027/fast-sigmoid-algorithm
-	setForward(getInputA()->getForward().unaryExpr(&sigmoid));
+	setForward(getInputA()->getForward().unaryExpr(std::ref(sigmoid)));
 };
 
 void Sigmoid::backwards() {
 	auto tmp = getForward();
 	tmp.setOnes();
-
 	auto dSigmoid = getForward().cwiseProduct(tmp - getForward());
 	getInputA()->setCurrentGradients(getCurrentGradients().cwiseProduct(dSigmoid));
 
