89385880f4c8f5c673ea220fb36dcdde4f432b52
ga96tul <ga96tul@mytum.de>
2019-07-31 23:33:07 +0200

    added tests for graph class added doxygen for Max Pool and Operations Factory

--- library/ComputationalGraph/Operations/include/OperationsFactory.hpp
+++ library/ComputationalGraph/Operations/include/OperationsFactory.hpp
@@ -17,49 +17,51 @@
 #include <ReLuOp.hpp>
 #include <SigmoidOP.hpp>
 #include <SoftmaxOp.hpp>
+
 /*!
  * This class implements the Factory Pattern in a functional way.
- * Each creation function assures that an Operation is added to the graph
+ * Each creation function assures that an Operation is not only created but also added to the compute graph
+ * if the input Node is not already in the Graph this will fail with an runtime_error cause by the Graph class
  */
 class OperationsFactory {
 public:
 
-	/*
-	 * Normal Functions
-	 */
-	static std::shared_ptr<ConvolutionOp>
-	createConvolutionOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix filterMatrix,
-						int channels, int stride);
+    /*
+     * Normal Functions
+     */
+    static std::shared_ptr<ConvolutionOp>
+    createConvolutionOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix filterMatrix,
+                        int channels, int stride);
 
-	static std::shared_ptr<SummationOp>
-	createSummationOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix biasMatrix,
-					  int channel);
+    static std::shared_ptr<SummationOp>
+    createSummationOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix biasMatrix,
+                      int channel);
 
-	static std::shared_ptr<MultiplicationOp>
-	createMultiplicationOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix weightMatrix);
+    static std::shared_ptr<MultiplicationOp>
+    createMultiplicationOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix weightMatrix);
 
-	static std::shared_ptr<MaxPoolOp>
-	createMaxpoolOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, int kernelDim, int stride);
+    static std::shared_ptr<MaxPoolOp>
+    createMaxpoolOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, int kernelDim, int stride);
 
-	/*
-	 * LossFunctions
-	 */
-	static std::shared_ptr<MSEOp>
-	createMSEOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix labelMatrix);
+    /*
+     * LossFunctions
+     */
+    static std::shared_ptr<MSEOp>
+    createMSEOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix labelMatrix);
 
-	static std::shared_ptr<CrossEntropyOp>
-	createCrossEntropyOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix labelMatrix);
+    static std::shared_ptr<CrossEntropyOp>
+    createCrossEntropyOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, Matrix labelMatrix);
 
-	/*
-	 * Activation Functions
-	 */
-	static std::shared_ptr<ReLuOp> createReLuOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input);
+    /*
+     * Activation Functions
+     */
+    static std::shared_ptr<ReLuOp> createReLuOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input);
 
-	static std::shared_ptr<SigmoidOP>
-	createSigmoidOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input);
+    static std::shared_ptr<SigmoidOP>
+    createSigmoidOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input);
 
-	static std::shared_ptr<SoftmaxOp>
-	createSoftmaxOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, int amountClasses);
+    static std::shared_ptr<SoftmaxOp>
+    createSoftmaxOp(std::shared_ptr<Graph> graph, const std::shared_ptr<Node> input, int amountClasses);
 
 
 };
